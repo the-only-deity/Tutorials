28526708b53994b4ef9d3d111579540d37318c4f
Mercy Mukundi <mercy.n.mukundi@gmail.com>
2025-09-24 15:39:49 +0300

    Included user error handling options in the bill splitter app.

--- JavaScript/billSplitter.js
+++ JavaScript/billSplitter.js
@@ -5,12 +5,31 @@ let tipPercentage = Number(
 let numberOfPeople = Number(prompt("How many people are splitting the bill?"));
 
 function splitBill(bill, tipPercentage, numberOfPeople) {
-	let tipAmount = (tipPercentage / 100) * bill;
-	let totalBill = bill + tipAmount;
-	let amountPerPerson = totalBill / numberOfPeople;
-	amountPerPerson = amountPerPerson.toFixed(2);
+	let output;
+	let amountPerPerson;
 
-	let output = `Your total bill is: KES${bill}\nThe tip percentage you chose was: ${tipPercentage}%\nThe total amount to be paid is: KES${totalBill}\nYou are splitting the bill amongst ${numberOfPeople} people\nEach person should pay: KES${amountPerPerson}`;
+	if (
+		(Number.isNaN(bill) ||
+			Number.isNaN(tipPercentage) ||
+			Number.isNaN(numberOfPeople)) == true
+	) {
+		output = "Error: Invalid input entered.";
+	} else if (bill == 0) {
+		output = "Error: Bill amount cannot be zero. Please enter a valid amount.";
+	} else {
+		let tipAmount = (tipPercentage / 100) * bill;
+		let totalBill = bill + tipAmount;
+
+		if (numberOfPeople == 0) {
+			amountPerPerson = totalBill;
+			output = `Your total bill is: KES${bill}\nThe tip percentage you chose was: ${tipPercentage}%\nYour total amount to pay is: KES${totalBill}`;
+		} else {
+			amountPerPerson = totalBill / numberOfPeople;
+			amountPerPerson = amountPerPerson.toFixed(2);
+			output = `Your total bill is: KES${bill}\nThe tip percentage you chose was: ${tipPercentage}%\nThe total amount to be paid is: KES${totalBill}\nYou are splitting the bill amongst ${numberOfPeople} people\nEach person should pay: KES${amountPerPerson}`;
+		}
+	}
+
 	return output;
 }
 
